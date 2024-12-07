7244edc64225c08a4e18d4d03303e70bc54d545d
Daniil Haranski <wheconn@gmail.com>
2024-12-07 20:09:59 +0300

    added a some features

--- shop-static-site/main/views.py
+++ shop-static-site/main/views.py
@@ -5,4 +5,8 @@ from django.shortcuts import render
 def index(request):
     return render(request,'main/index.html')
 
+
+def about(request):
+    return render(request,'main/index.html')
+
 # Create your views here.
