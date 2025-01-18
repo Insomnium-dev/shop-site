bf50f1cdc317f321bc46f9959412830eb3b66ed9
Daniil Haranski <wheconn@gmail.com>
2025-01-18 18:02:50 +0300

    added a pagination

--- shop-static-site/goods/views.py
+++ shop-static-site/goods/views.py
@@ -1,4 +1,5 @@
 from ctypes import cast
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from django.template import context
 from django.template.base import constant_string
@@ -7,16 +8,20 @@ from django.template.defaultfilters import truncatechars
 # Create your views here.
 from goods.models import Products
 
-def catalog(request, category_slug):
+def catalog(request, category_slug, page=1):
 
     if category_slug == 'all':
         goods = Products.objects.all()
     else:
         goods = Products.objects.filter(category__slug=category_slug)
     
+    paginator = Paginator(goods, 8)
+    current_page = paginator.page(page)
+    slug_url = category_slug
 
     context ={
-        'products':goods
+        'products':current_page,
+        'slug':slug_url
     }
     return render(request, 'goods/catalog.html',context=context)
 
