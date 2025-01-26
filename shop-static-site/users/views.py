41ddfc9a765947f15d5f7cfd330710374830905a
Daniil Haranski <wheconn@gmail.com>
2025-01-26 21:50:37 +0300

    start edit forms

--- shop-static-site/users/views.py
+++ shop-static-site/users/views.py
@@ -1,24 +1,44 @@
 from operator import rshift
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.contrib import auth
+from django.urls import reverse
+
+from users.forms import UserLoginForm
+
+
 
 # Create your views here.
 def login(request):
-    context={
-        
+    if request.method=="POST":
+        form=UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST["username"]
+            password = request.POST["password"]
+            user = auth.authenticate(request, username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('main:index'))
+    else:
+        form = UserLoginForm()
+
+
+
+    context = {
+        'form': form
     }
-    return render(request,'users/login.html',context)
+    return render(request, "users/login.html", context)
+
 
 def registration(request):
-    context={
-        
-    }
-    return render(request,'users/registration.html',context)
+    context = {}
+    return render(request, "users/registration.html", context)
+
 
 def profile(request):
-    context={
-        
-    }
-    return render(request,'users/profile.html',context)
+    context = {}
+    return render(request, "users/profile.html", context)
+
 
 def logout(request):
     pass
