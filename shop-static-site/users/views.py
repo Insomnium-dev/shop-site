cdc8a4293a1af5f139ee8911d66bf61d193fcfdc
Daniil Haranski <wheconn@gmail.com>
2025-01-27 22:04:56 +0300

    added a registration form

--- shop-static-site/users/views.py
+++ shop-static-site/users/views.py
@@ -1,37 +1,42 @@
 from operator import rshift
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.contrib import auth
 from django.urls import reverse
 
-from users.forms import UserLoginForm
-
+from users.forms import UserLoginForm, UserRegistrationForm
 
 
 # Create your views here.
 def login(request):
-    if request.method=="POST":
-        form=UserLoginForm(data=request.POST)
+    if request.method == "POST":
+        form = UserLoginForm(data=request.POST)
         if form.is_valid():
             username = request.POST["username"]
             password = request.POST["password"]
             user = auth.authenticate(request, username=username, password=password)
             if user:
                 auth.login(request, user)
-                return HttpResponseRedirect(reverse('main:index'))
+                return HttpResponseRedirect(reverse("main:index"))
     else:
         form = UserLoginForm()
 
-
-
-    context = {
-        'form': form
-    }
+    context = {"form": form}
     return render(request, "users/login.html", context)
 
 
 def registration(request):
-    context = {}
+    if request.method == "POST":
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.instance
+            auth.login(request, user)
+            return HttpResponseRedirect(reverse("main:index"))
+    else:
+        form = UserRegistrationForm()
+
+    context = {"form": form}
     return render(request, "users/registration.html", context)
 
 
@@ -41,4 +46,5 @@ def profile(request):
 
 
 def logout(request):
-    pass
+    auth.logout(request)
+    return redirect(reverse("main:index"))
