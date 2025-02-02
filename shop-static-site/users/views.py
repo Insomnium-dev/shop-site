bbc48fa4ccab53534e3dfde010dd6a6b32df199f
Daniil Haranski <wheconn@gmail.com>
2025-02-02 16:39:27 +0300

    added a remove from cart feature

--- shop-static-site/users/views.py
+++ shop-static-site/users/views.py
@@ -22,7 +22,8 @@ def login(request):
             if user:
                 auth.login(request, user)
                 # messages.success(request, f'{username}, вы успешно авторизованы!')
-                if request.POST.get('next', None):
+                redirect_page=request.POST.get('next', None)
+                if redirect_page and redirect_page!=reverse('users:logout'):
                     return HttpResponseRedirect(request.POST.get('next'))
             
             return HttpResponseRedirect(reverse("main:index"))
