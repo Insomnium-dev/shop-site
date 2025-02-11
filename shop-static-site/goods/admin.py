922aab842755b4802ca6e593829fc454f5151a40
Daniil Haranski <wheconn@gmail.com>
2025-02-11 21:20:04 +0300

    edit a admin panel

--- shop-static-site/goods/admin.py
+++ shop-static-site/goods/admin.py
@@ -4,12 +4,17 @@ from django.contrib import admin
 from goods.models import Categories, Products
 
 
-
-
 @admin.register(Categories)
 class CategoriesAdmin(admin.ModelAdmin):
-    prepopulated_fields={'slug':('name',)}
+    prepopulated_fields = {"slug": ("name",)}
+    list_display = ['name']
+
 
 @admin.register(Products)
 class ProductsAdmin(admin.ModelAdmin):
-    prepopulated_fields={'slug':('name',)}
+    prepopulated_fields = {"slug": ("name",)}
+    list_display = ["name", "price", "discount"]
+    list_editable = ["discount"]
+    search_fields = ["name", "description"]
+    list_filter = ["discount", "category"]
+    fields = ["name", "category", "slug", "description", "image", ("price", "discount")]
--- shop-static-site/goods/models.py
+++ shop-static-site/goods/models.py
@@ -1,16 +1,14 @@
-from email.mime import image
-from enum import unique
-from os import name
-from tabnanny import verbose
-from unicodedata import category
-from unittest.util import _MAX_LENGTH
 from django.db import models
 from django.forms import CharField
+from django.urls import reverse
+
 
 # Create your models here.
 class Categories(models.Model):
-    name = models.CharField(max_length = 150, unique = True, verbose_name="Название")
-    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name="URL")
+    name = models.CharField(max_length=150, unique=True, verbose_name="Название")
+    slug = models.SlugField(
+        max_length=200, unique=True, blank=True, null=True, verbose_name="URL"
+    )
 
     class Meta:
         db_table = "category"
@@ -20,29 +18,45 @@ class Categories(models.Model):
     def __str__(self):
         return self.name
 
+
 class Products(models.Model):
-    name = models.CharField(max_length = 150, unique = True, verbose_name="Название")
-    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name="URL")
-    price = models.DecimalField(default=0.00, max_digits=7, decimal_places=2, verbose_name="Цена")
-    image = models.ImageField(upload_to="goods_images",blank=True, null=True, verbose_name="Изображение")
-    category = models.ForeignKey(to=Categories,on_delete=models.CASCADE, verbose_name='Категория')
-    description = models.TextField(max_length=1000,unique=True,blank=True,null=True,verbose_name="Описание")
-    discount = models.DecimalField(default=0.00, max_digits=4, decimal_places=2, verbose_name="Скидка в %")
+    name = models.CharField(max_length=150, unique=True, verbose_name="Название")
+    slug = models.SlugField(
+        max_length=200, unique=True, blank=True, null=True, verbose_name="URL"
+    )
+    price = models.DecimalField(
+        default=0.00, max_digits=7, decimal_places=2, verbose_name="Цена"
+    )
+    image = models.ImageField(
+        upload_to="goods_images", blank=True, null=True, verbose_name="Изображение"
+    )
+    category = models.ForeignKey(
+        to=Categories, on_delete=models.CASCADE, verbose_name="Категория"
+    )
+    description = models.TextField(
+        max_length=1000, unique=True, blank=True, null=True, verbose_name="Описание"
+    )
+    discount = models.DecimalField(
+        default=0.00, max_digits=4, decimal_places=2, verbose_name="Скидка в %"
+    )
 
     class Meta:
         db_table = "product"
         verbose_name = "Продукт"
         verbose_name_plural = "Продукты"
-        
+
     def __str__(self):
         return self.name
-    
+
     def display_id(self):
         return f"{self.id:05}"
-    
+
     def calc_discount(self):
         if self.discount:
-            return round(self.price - self.price*self.discount/100, 2)
-        
-        return self.price
+            return round(self.price - self.price * self.discount / 100, 2)
 
+        return self.price
+    
+    def get_absolute_url(self):
+        return reverse("goods:product", kwargs={"product_slug": self.slug})
+    
